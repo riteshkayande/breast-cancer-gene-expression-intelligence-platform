eda7fb724f205a0bea8b290fde235d97316e1424
Ritesh Kayande <teshri60@gmail.com>
2026-05-12 16:49:43 +0530

    Add Streamlit application

---
+++ app/streamlit_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("AI-Driven Breast Cancer Gene Expression Intelligence Platform")
+
+st.write("Breast cancer prediction using gene expression data and machine learning.")
