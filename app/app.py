99f9b297ad0809c94e176e37e9d5a105fced19b8
Ritesh Kayande <teshri60@gmail.com>
2026-05-12 19:14:35 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,22 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Breast Cancer AI Platform", layout="wide")
+st.title("🧬 Breast Cancer AI Platform")
 
-st.title("🧬 Breast Cancer Gene Expression Platform")
+file = st.file_uploader("Upload CSV")
 
-st.success("App is running successfully 🎉")
-
-uploaded_file = st.file_uploader("Upload dataset (CSV)", type=["csv"])
-
-if uploaded_file:
-    df = pd.read_csv(uploaded_file)
-
-    st.write("### Data Preview")
+if file:
+    df = pd.read_csv(file)
     st.write(df.head())
-
-    st.write("### Shape")
-    st.write(df.shape)
-
-    st.write("### Basic Info")
-    st.write(df.describe())
